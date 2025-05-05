718c0f338234efe7d7443333f5d9afa16926d830
dipak1920basnet <dipak1920basnet@gmail.com>
2025-05-05 09:53:56 +0545

    view items from categories

--- commerce/commerce/auctions/views.py
+++ commerce/commerce/auctions/views.py
@@ -4,12 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from .models import User
+from .models import User, Category, Auction_listing, Comment, Bid
 from django.contrib import messages
 
 def index(request):
-    return render(request, "auctions/index.html")
-
+    return render(request, "auctions/index.html",
+                      {
+                          "active_list":Auction_listing.objects.all(),
+                          "Category":Category.objects.all(),
+                      })
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +64,57 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+
+
+
+## Creating a listing
+@login_required
+def create(request):
+    return render(request, "auctions/create.html", {
+        "Category":Category.objects.all(),
+    })
+
+## Get created listing data
+@login_required
+def create_listing(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        url = request.POST.get("url")
+        category = request.POST.get("Category")
+        starting_bid = int(request.POST.get("min_bid"))
+
+        # Create category and save category if category not in category
+        category_instance = Category.objects.get(category_field=category)
+        owner_instance = User.objects.get(username=request.user)
+        #
+        # if not category_instance:
+            # Category.objects.create(category_field = category)
+        # category_instance = Category.objects.filter(category_field = category).first()
+        #Get the name of user 
+        # user = User().objects.all()
+        listing = Auction_listing.objects.create(title = title, description = description,starting_bid = starting_bid, image_url=url, category=category_instance, owner=owner_instance)
+        # if listing.is_valid():
+        listing.save()
+        
+        return HttpResponseRedirect(reverse('index'))
+    
+
+    
+## View list of categories 
+def categories(request):
+    return render(request, "auctions/categories.html",
+                  {
+                      "Category_List": Category.objects.all()
+                  })
+
+## View item from categories:
+def view_item_categories(request, category_name):
+    category = Category.objects.get(category_field = category_name)
+    return render(request, "auctions/category_item.html",
+                  {
+                      "category_name":category_name,
+                      "category_name_list": category.category.all()
+                  })
